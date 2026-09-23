522a55bba7f518ebe8469416eddfaa9146396cc6
gnix22 <gjnix22@gmail.com>
2026-09-22 22:25:40 -0400

    got some outputs working

--- cc/apriori/aprioriAlgorithm.h
+++ cc/apriori/aprioriAlgorithm.h
@@ -2,17 +2,54 @@
 #define APRIORI_ALGORITHM_H
 #include<iostream>
 #include<vector>
+#include<algorithm>
+#include<iostream>
+#include<set>
 template<typename T>
 class Apriori{
     private:
         std::vector<T> _items;
         bool _isFrequent;
         int _totalSampleSize;
-        int _supCount();
         std::vector<T> _aprioriGen(std::vector<T> prevFrequentItemsets);
+        int _kCount;
     public:
-        Apriori(std::vector<T> items); // constructor
-        std::vector<T> frequentItemSetGen(double minSup);
+        // constructor
+        Apriori(std::vector<T> items){
+            _items = items;
+            _isFrequent = true;
+            _totalSampleSize = _items.size();
+        }
+        std::vector<std::vector<T>> frequentItemSetGen(double minSup){
+            std::vector<std::vector<T>> frequentItemsets;
+            std::set<T> unique_items(_items.begin(), _items.end());
+            for(T item : unique_items){
+                if(std::count(_items.begin(), _items.end(), item) >= _totalSampleSize * minSup){
+                    frequentItemsets.push_back(std::vector{item});
+                }
+            }
+            return frequentItemsets;
+        }
+        void printIteration(int k, std::vector<std::vector<T>>currFreqItemset){
+            if(currFreqItemset.empty()){
+                std::cout << "no itemsets created that matched given support. try lowering support.";
+            }else{
+                std::cout << "frequent " << k << "-itemsets: ";
+                for(int i=0; i<currFreqItemset.size(); i++){
+                    std::cout << "{";
+                    for(int j=0; j<currFreqItemset[i].size(); j++){
+                        std::cout << currFreqItemset[i].at(j);
+                        if(j < currFreqItemset[i].size() - 1){
+                            std::cout << ", ";
+                        }
+                    }
+                    if(i < currFreqItemset.size()-1){
+                        std::cout << "}, ";
+                    } else{
+                        std::cout << "}";
+                    }
+                }
+            }
+        }
 };
-#include"aprioriAlgorithm.cc"
 #endif
